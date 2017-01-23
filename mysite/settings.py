cd2c60b9ff254af03f5ffcf2ee47e736c76402c3
pluttik <pieternella.luttikhuizen@nioz.nl>
2017-01-23 14:45:52 +0100

    changed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ with open('etc/secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.pythonanywhere.com']
 
 
 # Application definition
